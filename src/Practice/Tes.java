1eaf8da143263308ca0a506f81fabef6864891ce
jasonenkristo123 <jasonenkristo@gmail.com>
2026-03-19 23:13:23 +0700

    inheritance

--- src/Practice/Tes.java
+++ src/Practice/Tes.java
@@ -6,12 +6,27 @@ public class Tes {
 
 class HewanParent {
     int x = 10;
+    String namaHewan;
+    int umurHewan;
+
+    public HewanParent(String nama, int umur) {
+        this.namaHewan = nama;
+        this.umurHewan = umur;
+    }
+
     public void suaraHewan() {
         System.out.println("Suara hewan");
     }
 }
 
 class HewanChild extends HewanParent {
+    String namaAnak;
+    int umurAnak;
+    public HewanChild(String nama, int umur) {
+        super(nama, umur);
+        this.namaAnak = nama;
+        this.umurAnak = umur;
+    }
     public void suaraKucing() {
         System.out.println("Meow meow");
         System.out.println(super.x);
@@ -21,8 +36,12 @@ class HewanChild extends HewanParent {
 
 class Panggil {
     public static void main (String[] args) {
-        HewanChild child = new HewanChild();
-        child.suaraHewan();
-        child.suaraKucing();
+        HewanParent parent = new HewanParent("Anjing", 5);
+        HewanChild child = new HewanChild("Kucing", 2);
+        System.out.println(parent.namaHewan);
+        System.out.println(parent.umurHewan);
+        System.out.println(child.namaAnak);
+        System.out.print(child.umurAnak);
+
     }
 }
